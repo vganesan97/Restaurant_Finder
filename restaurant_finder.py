4aefbc3fe92c8116fce046ced8693b97082e1aa4
vganesan97 <vishaalganesan@Vishaals-MacBook-Pro.local>
2016-06-10 01:05:55 -0700

    second commit

--- restaurant_finder.py
+++ restaurant_finder.py
@@ -5,12 +5,9 @@ from twilio.rest import TwilioRestClient
 
 
 
-locu_api='30bf9d23736f562297e8c86b2b933214e7d119ea'
-account_sid_twilio="{{AC382aa7d2a753e94aea2f916a7746ca2f}}"
-auth_token_twilio="[02bb57b080d6b66a99a8033a8b7c10f3]"
-
-client = TwilioRestClient(account='AC382aa7d2a753e94aea2f916a7746ca2f',
-                              token='02bb57b080d6b66a99a8033a8b7c10f3')
+locu_api='api token'
+account_sid_twilio="{{sid}}"
+auth_token_twilio="[token]"
 
 def locusearch(city, region, zipp, category):
 	api_key=locu_api
@@ -19,11 +16,10 @@ def locusearch(city, region, zipp, category):
 	final_url=url+ "locality=" + locality + "&region"+region+ "&postal_code="+ zipp + "&category=" +category+ "&api_key=" + api_key
 	json_obj=urllib2.urlopen(final_url)
 	data=json.load(json_obj) 
-	x=[]
 	s=""
-	client = TwilioRestClient(account='AC382aa7d2a753e94aea2f916a7746ca2f',token='02bb57b080d6b66a99a8033a8b7c10f3')
+	client = TwilioRestClient(account='sid',token='token')
 	for item in data['objects']:
-		s+=item['name']+ " "+item['phone'] + "\n"
+		s+=item['name']+ " " +item['phone'] + "\n"
 	client.messages.create(
 		from_="+12155844319",
 		to="(609) 462-4702", 
